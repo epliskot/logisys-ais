8ac576d5cba3e1f6eb1d23e57e95320fbbb7bb53
epliskot <jon.dellnes@nmbu.no>
2017-04-21 10:14:05 +0200

    Switching filepath
    from workspace to home

---
+++ src/main/java/org/epliskot/logsys/logisysais/LogisysAisApplication.java
@@ -0,0 +1,12 @@
+package org.epliskot.logsys.logisysais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisysAisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisysAisApplication.class, args);
+	}
+}
---
+++ src/test/java/org/epliskot/logsys/logisysais/LogisysAisApplicationTests.java
@@ -0,0 +1,16 @@
+package org.epliskot.logsys.logisysais;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogisysAisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
